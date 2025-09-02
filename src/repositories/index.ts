a2ca397a1f51b520373805836f123ea2b37241ce
bharatbhusal <bharatbhusal78@gmail.com>
2025-09-02 18:24:39 +0530

    feat: export otpRepository from index file

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,3 +2,4 @@ export * from "./userRepository";
 export * from "./apiKeyRepository";
 export * from "./usageCurrentRepository";
 export * from "./auditLogRepository";
+export * from "./otpRepository";
